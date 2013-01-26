fc0101f95929949a8c2b2ad211891baaf649b797
Dian Nikolov <dian.nikolov91@gmail.com>
2013-01-26 20:10:27 +0200

    First Upload

---
+++ CarService/CarService/Controllers/RepairCardController.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarService.DAL;
+using WebMatrix.WebData;
+using CarService.ViewModels;
+
+namespace CarService.Controllers
+{   [Authorize]
+    public class RepairCardController : Controller
+    {
+        private CarServiceEntities db = new CarServiceEntities();
+
+        //
+        // GET: /RepairCard/
+
+        public ActionResult Index(FormCollection fc, string searchString, string entryDate)
+        {
+            ViewBag.CurrentUserId = WebSecurity.CurrentUserId;
+            ViewBag.IsAdmin = db.UserProfiles.Where(u => u.UserName == "admin" || u.UserName == "Admin")
+                .Select(u => u.UserId)
+                .Single() == WebSecurity.CurrentUserId; 
+
+            var repairCards = db.RepairCards
+                .Include(r => r.Car)
+                .Include(r => r.Employee)
+                .Include(r => r.UserProfile);
+
+            if (!String.IsNullOrEmpty(searchString) && null != entryDate)
+            {
+                List<RepairCard> searchedRepairCards = new List<RepairCard>();
+
+                foreach (var repairCard in repairCards)
+                {
+                    if ((entryDate == repairCard.EntryDate.ToString()) 
+                        && (repairCard.Car.RegistryNumber.Contains(searchString) || repairCard.Car.FrameNumber.ToString().Contains(searchString)))
+                    {
+                        searchedRepairCards.Add(repairCard);
+                    }
+                }
+                PopulateEntryDatesList();
+                return View(searchedRepairCards);
+            }
+
+            PopulateEntryDatesList();
+            return View(repairCards.ToList());
+        }
+
+        public void PopulateEntryDatesList()
+        {
+            HashSet<string> entryDates = new HashSet<string>();
+            foreach (var repairCard in db.RepairCards)
+            {
+                if (!entryDates.Contains(repairCard.EntryDate.ToString()))
+                {
+                    entryDates.Add(repairCard.EntryDate.ToString());
+                }
+            }
+
+            ViewBag.EntryDates = entryDates.OrderBy(x => x); 
+        }
+        //
+        // GET: /RepairCard/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            RepairCard repaircard = db.RepairCards.Find(id);
+            if (repaircard == null)
+            {
+                return HttpNotFound();
+            }      
+
+            return View(repaircard);
+        }
+
+        //
+        // GET: /RepairCard/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.CarId = new SelectList(db.Cars, "Id", "RegistryNumber");
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName");
+            ViewBag.SpareParts = db.SpareParts.ToList();
+            return View();
+        }
+
+        //
+        // POST: /RepairCard/Create
+
+        [HttpPost]
+        public ActionResult Create(RepairCard repairCard, string[] selectedParts)
+        {
+            //RepairCard repairCard = new RepairCard();
+            //if (TryUpdateModel(repairCard, "", null, excludeProperties: new string[] { "SpareParts", "EntryDate", "PartsPrice", "UserId", "Id" }))
+            //{
+                try
+                {
+                    repairCard.PartsPrice = 0;
+
+                    if (selectedParts == null)
+                    {
+                        repairCard.SpareParts = new List<SparePart>();
+                    }
+                    else
+                    {
+                        var selectedPartsHS = new HashSet<string>(selectedParts);
+
+                        foreach (var sparePart in db.SpareParts)
+                        {
+                            if (selectedPartsHS.Contains(sparePart.Id.ToString()))
+                            {
+                                repairCard.SpareParts.Add(sparePart);
+                                repairCard.PartsPrice += sparePart.Price;
+                            }
+                        }
+
+                        repairCard.UserId = WebSecurity.CurrentUserId;
+                        repairCard.EntryDate = DateTime.Now;
+                        db.RepairCards.Add(repairCard);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (DataException)
+                {
+                    //Log the error (add a variable name after DataException)
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            //}
+            ViewBag.CarId = new SelectList(db.Cars, "Id", "RegistryNumber", repairCard.CarId);
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName", repairCard.EmployeeId);
+            ViewBag.SpareParts = db.SpareParts.ToList();
+            return View(repairCard);
+        }
+
+        //
+        // GET: /RepairCard/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            RepairCard repaircard = db.RepairCards.Find(id);
+            if (repaircard == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CarId = new SelectList(db.Cars, "Id", "RegistryNumber", repaircard.CarId);
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName", repaircard.EmployeeId);
+
+            InitializeEditViewModel(repaircard);
+
+            return View(repaircard);
+        }
+
+        public void InitializeEditViewModel(RepairCard repaircard)
+        {
+            var allSpareParts = db.SpareParts;
+            var repairCardParts = new HashSet<int>(repaircard.SpareParts.Select(c => c.Id));
+            var viewModel = new List<SelectedPartData>();
+            foreach (var part in allSpareParts)
+            {
+                viewModel.Add(new SelectedPartData
+                {
+                    Id = part.Id,
+                    Price = part.Price,
+                    PartName = part.PartName,
+                    Selected = repairCardParts.Contains(part.Id)
+                });
+            }
+            ViewBag.SpareParts = viewModel;
+        }
+
+        //
+        // POST: /RepairCard/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection formCollection, string[] selectedParts)
+        {
+            var repairCardToUpdate = db.RepairCards
+                                       .Include(i => i.SpareParts)
+                                       .Where(i => i.Id == id)
+                                       .Single();
+
+            if (TryUpdateModel(repairCardToUpdate, "", null, excludeProperties: new string[] { "SpareParts" }))
+            {
+                try
+                {
+                    UpdateRepairCardSpareParts(selectedParts, repairCardToUpdate);
+
+                    repairCardToUpdate.PartsPrice = 0;
+                    foreach (var sparePart in repairCardToUpdate.SpareParts)
+                    {
+                        repairCardToUpdate.PartsPrice += sparePart.Price;
+                    }
+
+                    db.Entry(repairCardToUpdate).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (DataException)
+                {
+                    //Log the error (add a variable name after DataException)
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            ViewBag.CarId = new SelectList(db.Cars, "Id", "RegistryNumber", repairCardToUpdate.CarId);
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName", repairCardToUpdate.EmployeeId);
+            InitializeEditViewModel(repairCardToUpdate);
+            return View(repairCardToUpdate);
+        }
+
+        private void UpdateRepairCardSpareParts(string[] selectedParts, RepairCard repairCardToUpdate)
+        {
+
+
+            if (selectedParts == null)
+            {
+                repairCardToUpdate.SpareParts = new List<SparePart>();
+                return;
+            }
+
+            var selectedPartsHS = new HashSet<string>(selectedParts);
+            var repairCardSpareParts = new HashSet<int>
+                (repairCardToUpdate.SpareParts.Select(c => c.Id));
+            foreach (var sparePart in db.SpareParts)
+            {
+                if (selectedPartsHS.Contains(sparePart.Id.ToString()))
+                {
+                    if (!repairCardSpareParts.Contains(sparePart.Id))
+                    {
+                        repairCardToUpdate.SpareParts.Add(sparePart);
+                    }
+                }
+                else
+                {
+                    if (repairCardSpareParts.Contains(sparePart.Id))
+                    {
+                        repairCardToUpdate.SpareParts.Remove(sparePart);
+                    }
+                }
+            }
+        }
+        //
+        // GET: /RepairCard/RepairFinish/5
+
+        public ActionResult RepairFinish(int id = 0)
+        {
+            RepairCard repaircard = db.RepairCards.Find(id);
+            if (repaircard == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CarId = new SelectList(db.Cars, "Id", "RegistryNumber", repaircard.CarId);
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName", repaircard.EmployeeId);
+            ViewBag.UserId = new SelectList(db.UserProfiles, "UserId", "UserName", repaircard.UserId);
+            return View(repaircard);
+        }
+
+        //
+        // POST: /RepairCard/RepairFinish/5
+
+        [HttpPost]
+        public ActionResult RepairFinish(RepairCard repaircard)
+        {
+            if (ModelState.IsValid)
+            {
+                if (repaircard.TotalPrice > repaircard.PartsPrice)
+                { 
+                repaircard.RepairFinishDate = DateTime.Now;
+                db.Entry(repaircard).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+                }
+            }
+
+            ViewBag.CarId = new SelectList(db.Cars, "Id", "RegistryNumber", repaircard.CarId);
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName", repaircard.EmployeeId);
+            ViewBag.UserId = new SelectList(db.UserProfiles, "UserId", "UserName", repaircard.UserId);
+            return View(repaircard);
+        }
+
+        //
+        // GET: /RepairCard/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            RepairCard repaircard = db.RepairCards.Find(id);
+            if (repaircard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(repaircard);
+        }
+
+        //
+        // POST: /RepairCard/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            RepairCard repaircard = db.RepairCards.Find(id);
+            db.RepairCards.Remove(repaircard);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CarService/CarService/Views/RepairCard/Index.aspx
@@ -0,0 +1,109 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<IEnumerable<CarService.DAL.RepairCard>>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    Index
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+<h2>Index</h2>
+
+<p>
+    <%: Html.ActionLink("Create New", "Create") %>
+</p>
+
+
+<%using (Html.BeginForm()){  %>   
+
+        <div>
+        <strong>Entry Date: </strong> <select name="entryDate">
+                                        <% foreach (var date in ViewBag.EntryDates) {%>
+                                            <option><%: date %></option>
+                                        <% } %>
+                                      </select>
+        <p> <%: Html.TextBox("SearchString") %>  <input type="submit" value="Search" /> 
+        </p> 
+        </div>
+
+<%}%>
+
+<%using (Html.BeginForm()){  %>   
+
+        <div>
+        <strong>Start:</strong> <select name="startDate">
+            <% foreach (var date in ViewBag.EntryDates) {%>
+                      <option><%: date %></option>
+            <% } %>
+        </select>
+        <strong>End:</strong> <select name="endDate">
+                <% foreach (var date in ViewBag.EntryDates)
+                   {%>
+                <option><%: date %></option>
+                <% } %>
+        </select>
+        <input type="submit" value="Search" /> 
+        </div>
+
+<%}%>
+
+<table class="sortable">
+    <tr>
+        <th>
+            Entry Date
+        </th>
+        <th>
+            Repair Finish Date
+        </th>
+        <th>
+            Employee
+        </th>
+        <th>
+            Car Registry Number
+        </th>
+
+        <th>
+            Total Cost
+        </th>
+
+        <th></th>
+    </tr>
+
+<% foreach (var item in Model) { %>
+    <tr>
+        <td>
+            <%: Html.DisplayFor(modelItem => item.EntryDate) %>
+        </td>
+        <td>
+            <%: Html.DisplayFor(modelItem => item.RepairFinishDate) %>
+        </td>
+        <td>
+            <%: Html.DisplayFor(modelItem => item.Employee.FirstName) %>
+        </td>
+        <td>
+            <%: Html.DisplayFor(modelItem => item.Car.RegistryNumber) %>
+        </td>
+
+        <td>
+            <%: Html.DisplayFor(modelItem => item.TotalPrice) %>
+        </td>
+
+        <td>
+            <%: Html.ActionLink("Details", "Details", new { id=item.Id }) %> 
+            <% if (!(!(item.RepairFinishDate == null) || !(item.UserId == ViewBag.CurrentUserId || ViewBag.IsAdmin))) { %>
+               | <%: Html.ActionLink("Edit", "Edit", new { id=item.Id }) %> |
+                <%: Html.ActionLink("Finish Repair", "RepairFinish", new { id=item.Id }) %>
+            <% } %>
+        </td>
+    </tr>
+<% } %>
+
+</table>
+
+
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="FeaturedContent" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content4" ContentPlaceHolderID="ScriptsSection" runat="server">
+</asp:Content>
